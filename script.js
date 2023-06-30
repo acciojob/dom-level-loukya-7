cbc60a66e492001c4e285aab462bf413e96e0438
loukya-7 <131159563+loukya-7@users.noreply.github.com>
2023-06-30 20:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.addEventListener('DOMContentLoaded', function() {
+  const element = document.getElementById('level');
+  let level = 0;
+  let currentElement = element;
+
+  while (currentElement.parentNode) {
+    currentElement = currentElement.parentNode;
+    level++;
+  }
+
+  alert("The level of the element is: " + level);
+});
